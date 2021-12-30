b096c6d14f09fd6bc61ce342ac73b2b257cfe72a
Pritom Sarkar <pritomsarkar300@gmail.com>
2021-12-30 21:17:45 +0600

    First Commit

---
+++ Hellow-payment/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Hellow_payment.Models;
+using Hellow_payment.Services;
+using Braintree;
+
+namespace Hellow_payment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IBraintreeService _braintreeService;
+
+        public HomeController(IBraintreeService braintreeService)
+        {
+            _braintreeService = braintreeService;
+        }
+
+
+
+
+        public IActionResult Index()
+        {
+            //var gateway = _braintreeService.GetGateway();
+            //var clientToken = gateway.ClientToken.Generate();
+            //ViewBag.ClientToken = clientToken;
+
+            //return View();
+            var gateway = _braintreeService.GetGateway();
+            var clientToken = gateway.ClientToken.Generate();
+            ViewBag.ClientToken = clientToken;
+
+            var data = new BookPurchaseVM
+            {
+                Id = 2,
+                Description = "Hellow man",
+                Author = "Me",
+                Thumbnail = "This is thumbnail",
+                Title ="This is title",
+                Price = "230",
+                Nonce = ""
+            };
+
+            return View(data);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+
+        [HttpPost]
+        public IActionResult Create(BookPurchaseVM model)
+        {
+            var gateway = _braintreeService.GetGateway();
+            
+
+            var request = new TransactionRequest
+            {
+                Amount = Convert.ToDecimal("250"),
+                PaymentMethodNonce = model.Nonce,
+                Options = new TransactionOptionsRequest
+                {
+                    SubmitForSettlement = true
+                }
+            };
+
+            Result<Transaction> result = gateway.Transaction.Sale(request);
+
+            if (result.IsSuccess())
+            {
+                return View("Success");
+            }
+            else
+            {
+                return View("Failure");
+            }
+        }
+    }
+}
---
+++ Hellow-payment/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hellow_payment.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
